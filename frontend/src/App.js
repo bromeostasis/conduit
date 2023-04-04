fe57a0686fcd42a5b113a8576dff8fb95445ce94
Evan Snyder <evan@larky.com>
2023-04-04 11:29:31 -0700

    Tab spacing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,46 +4,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [nextSelectors, setNextSelectors] = useState([])
+    const [nextSelectors, setNextSelectors] = useState([])
 
-  useEffect(() => {
-      fetch("/get_next_selectors", {
-        method: 'POST',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify([])
-      }).then((res) =>
-          res.json().then((data) => {
+    useEffect(() => {
+        fetch("/get_next_selectors", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify([])
+        }).then((res) =>
+        res.json().then((data) => {
             setNextSelectors(data)
-          })
-      );
-  }, []);
+        })
+        );
+    }, []);
 
-  const test = () => {
-    console.log('woo')
-  }
+    const test = () => {
+        console.log('woo')
+    }
 
-  return (
-    <div className="App">
-      Selectors: <br/>
-      {nextSelectors.map((selector) => {
-        const key = Object.keys(selector)[0]
-        const values = Object.values(selector)[0]
-        return (
-          <>
-            <label>{key}</label>
-            <select key={key} onChange={test}>
-              <option value={undefined}>~Select value~</option>
-            {values.map((value) => (
-              <option value={value}>{value}</option>
-            ))}
-            </select>
-          </>
-        )
-      })}
-    </div>
-  );
+    return (
+        <div className="App">
+        Selectors: <br/>
+        {nextSelectors.map((selector) => {
+            const key = Object.keys(selector)[0]
+            const values = Object.values(selector)[0]
+            return (
+                <>
+                <label>{key}</label>
+                <select key={key} onChange={test}>
+                <option value={undefined}>~Select value~</option>
+                {values.map((value) => (
+                    <option value={value}>{value}</option>
+                    ))}
+                </select>
+                </>
+                )
+        })}
+        </div>
+        );
 }
 
 export default App;
