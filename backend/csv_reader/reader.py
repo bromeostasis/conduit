a8c2769a9e75df65c45041d84823633c4b3a04e8
Evan Snyder <evan@larky.com>
2023-04-04 11:00:27 -0700

    Adds ability to filter csv based on input

--- backend/csv_reader/reader.py
+++ backend/csv_reader/reader.py
@@ -6,7 +6,24 @@ filename = os.path.join(dirname, 'data/ceiling-data.csv')
 
 df = pd.read_csv(filename) # TODO: dtypes? maybe don't do here to avoid overwrites.
 
+ORDERED_COLUMNS = ['Ceiling type','Deck construction','Attic ventilation','Attic fan','Radiant barrier','Roofing material','Roof color','Insulation','R-value','Extended Construction Numbers','Look-Up Construction Number','Notes']
+
 def get_next_results(columns_with_selections):
 
 	if columns_with_selections == []:
 		return [{'Ceiling type': df['Ceiling type'].unique().tolist()}]
+
+	filtered_df = filter_csv_with_selections(columns_with_selections)
+	next_column = ORDERED_COLUMNS[len(columns_with_selections)] # Since last two columns are extraneous and unselectable, this is safe
+
+	results = {}
+	results[next_column] = filtered_df[next_column].unique().tolist()
+	return [results]
+
+def filter_csv_with_selections(columns_with_selections):
+	first_selection = columns_with_selections[0]
+	key = list(first_selection.keys())[0]
+	query_string = f'`{key}` == "{first_selection[key]}"'
+	# for selection in filter_csv_with_selections:
+
+	return df.query(query_string)
--- backend/csv_reader/test/test_reader.py
+++ backend/csv_reader/test/test_reader.py
@@ -4,3 +4,16 @@ def test_get_first_column():
 	results = get_next_results([])
 
 	assert results == [{"Ceiling type": ["Ceiling below roof joists", "Ceiling on exposed beams", "Ceiling under attic or attic knee wall"]}]
+
+def test_get_second_column_simple():
+	results = get_next_results([{'Ceiling type': 'Ceiling on exposed beams'}])
+
+	assert results == [
+		{
+			'Deck construction': [
+				'1.5" wood',
+				'2" cement fiber slab',
+				'3" cement fiber slab'
+			]
+		}
+	]
