c844de94e252c0c86ed71b0842805fcba041ea44
Evan Snyder <evan@larky.com>
2023-04-04 09:54:54 -0700

    Initial setup: flask + react talking to each other.

---
+++ backend/server.py
@@ -0,0 +1,22 @@
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+  
+now = datetime.datetime.now()
+  
+# Initializing flask app
+app = Flask(__name__)
+  
+  
+# Route for seeing a data
+@app.route('/data')
+def get_time():
+    return [{
+        "String":"Celing type",
+        "Date":now,
+    }]
+  
+      
+# Running app
+if __name__ == '__main__':
+    app.run(debug=True)
